6a0de6ef79154a20a7af0e0e19e7519373d8bb95
ravine13 <kuriaderek@gmail.com>
2023-10-09 21:48:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+
+  const palindromeWord = palindrome(word);
+  if (word === palindromeWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
+function palindrome(word) {
+  return word.split("").reverse().join("");
+}
 /* 
   Add your pseudocode here
+ pass abba as an argument when invoking isPalindrome
+ and use the if condition and logical operators. 
 */
 
 /*
   Add written explanation of your solution here
+  i passed abba as an argument when invoking the function isPalindrome.
+  I then used if and else condtions along with the logical operators (strict equality)
+  to compare whether the value abba is a palindrome or not
 */
 
 // You can run `node index.js` to view these console logs
